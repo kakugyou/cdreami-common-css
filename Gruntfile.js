9316a3aa5367aafd88e7c868b8a389e720ae710c
kakugyou <guoxiao@cdreami.com>
2017-06-10 11:40:39 +0800

    init

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+'use strict';
+var fs = require('fs');
+
+require('./package.json');
+
+var gruntConfig = {};
+
+try {
+    gruntConfig = require('./grunt-config.js');
+} catch (e) {
+    gruntConfig = require('./grunt-config-sample.js');
+}
+
+
+module.exports = function (grunt) {
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
+
+    // Project configuration.
+    grunt.initConfig({
+        watch: {
+            styles: {
+                files: ["less/**"],
+                tasks: ["less:dev"],
+                options: {
+                    livereload: false, //default port 35729
+                    nospaces: true
+                }
+            },
+            resources: {
+                files: ["*",'!less/*'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+        less: {
+            dev: {
+                options: {
+                    compress: false,
+                    yuicompress: false,
+                    optimization: 2,
+                    sourceMap: true,
+                    sourceMapRootpath: gruntConfig.lessMapUrlPrefix,
+                    paths: ["less/"]
+                },
+                files: {
+                    "dist/style.css": "less/style.less",
+                }
+            },
+            prod: {
+                options: {
+                    compress: true,
+                    yuicompress: true,
+                    optimization: 2,
+                    sourceMap: false,
+                    paths: ["less/"]
+                },
+                files: {
+                    "dist/style.min.css": "less/style.less",
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['less:dev', 'watch']);
+    grunt.registerTask('build', ['less:dev','less:prod']);
+
+};
---
+++ grunt-config-sample.js
@@ -0,0 +1,4 @@
+//复制此文件为grunt-config.js，并修改为自已的配置(不必提交到git)
+module.exports = {
+    lessMapUrlPrefix:'/'
+};
